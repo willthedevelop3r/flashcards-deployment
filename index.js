b8da52088650a92e9ecd71c20132282ed03a901d
William Nguyen <williamnguyen773@gmail.com>
2023-07-17 07:57:17 -0500

    commented out cron

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const axios = require('axios');
-const cron = require('node-cron');
+// const axios = require('axios');
+// const cron = require('node-cron');
 
 const { PORT = 8080 } = process.env;
 
@@ -10,9 +10,9 @@ server.use(jsonServer.router('data/db.json'));
 
 server.listen(PORT);
 
-cron.schedule('*/14 * * * *', () => {
-  axios
-    .get('https://flashcards-backend-ju7k.onrender.com')
-    .then((response) => console.log(`Status: ${response.status}`))
-    .catch((error) => console.error(error));
-});
+// cron.schedule('*/14 * * * *', () => {
+//   axios
+//     .get('https://flashcards-backend-ju7k.onrender.com')
+//     .then((response) => console.log(`Status: ${response.status}`))
+//     .catch((error) => console.error(error));
+// });
