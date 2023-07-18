f9b3c486fa3d0768a44691e0fa4b590d6b251312
William Nguyen <williamnguyen773@gmail.com>
2023-07-17 19:47:25 -0500

    uninstalled axios and cron

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-// const axios = require('axios');
-// const cron = require('node-cron');
 
 const { PORT = 8080 } = process.env;
 
@@ -9,10 +7,3 @@ server.use(jsonServer.defaults());
 server.use(jsonServer.router('data/db.json'));
 
 server.listen(PORT);
-
-// cron.schedule('*/14 * * * *', () => {
-//   axios
-//     .get('https://flashcards-backend-ju7k.onrender.com')
-//     .then((response) => console.log(`Status: ${response.status}`))
-//     .catch((error) => console.error(error));
-// });
